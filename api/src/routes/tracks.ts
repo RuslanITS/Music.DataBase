7cb5800af574f38f6cc3b29664bda9fa1346788b
Ruslan <rcernov442@gmail.com>
2026-08-29 02:47:17 +0600

    add track filtering by album

--- api/src/routes/tracks.ts
+++ api/src/routes/tracks.ts
@@ -1,14 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
 import Track from "../models/Track";
-
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const track = await Track.find().populate('album');
+  const { album } = req.query;
 
-  res.send(track)
-})
+  if (typeof album === "string") {
+    const tracks = await Track.find({
+      album: new mongoose.Types.ObjectId(album),
+    }).populate("album");
+
+    return res.send(tracks);
+  }
+
+  const tracks = await Track.find().populate("album");
+
+  res.send(tracks);
+});
 
 router.get("/:id", async (req, res) => {
   const track = await Track.findById(req.params.id).populate('album');
