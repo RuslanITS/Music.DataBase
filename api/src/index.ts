bc094199c5f4919d4a21d0adf23864fd7cc8f602
Ruslan <rcernov442@gmail.com>
2026-08-28 21:51:16 +0600

    add albums and artist relationship

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import artistsRouter from "./routes/artists";
+import albumsRouter from "./routes/albums";
 
 const app = express();
 const port = 8000;
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/artists", artistsRouter);
+app.use("/albums", albumsRouter);
 
 const run = async () => {
   await mongoose.connect("mongodb://localhost:27017/music");
---
+++ api/src/routes/albums.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import Album from "../models/Album";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const albums = await Album.find().populate("artist");
+
+  res.send(albums);
+});
+
+router.get("/:id", async (req, res) => {
+  const album = await Album.findById(req.params.id).populate("artist");
+
+  res.send(album);
+});
+
+router.post("/", async (req, res) => {
+  const album = new Album({
+    name: req.body.name,
+    imageUrl: req.body.imageUrl,
+    year: req.body.year,
+    artist: req.body.artist,
+  })
+
+  await album.save();
+
+  res.send(album)
+})
+
+export default router
